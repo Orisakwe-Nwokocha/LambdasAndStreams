38ed9f4ab0ebe5f269bd866e968bda51fa4034c0
Orisakwe Nwokocha <orisakwenwokocha1@gmail.com>
2024-05-20 17:43:01 +0100

    added more functions

---
+++ README.md
@@ -0,0 +1,1 @@
+# Fundamental functional programming techniques and how they complement object-oriented programming.
---
+++ src/streams/chapter7Simplified/Example2.java
@@ -0,0 +1,12 @@
+package streams.chapter7Simplified;
+
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int[] values = {3, 0, 6, 1, 4, 8, 2, 5, 9};
+    }
+}
