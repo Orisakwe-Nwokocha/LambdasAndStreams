bd80bb660006af99fe9d618edb98a1a057c7ed4f
Orisakwe Nwokocha <orisakwenwokocha1@gmail.com>
2024-05-22 09:58:00 +0100

    added text files for the utility test

--- test/utils/UtilityTest.java
+++ test/utils/UtilityTest.java
@@ -6,34 +6,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class UtilityTest {
 
-//    @Test
-//    public void testNumberOfDigits() {
-//        String location = "C:\\Users\\User\\Videos\\Sample.txt";
-//        assertEquals(2, Utility.countNumberOfDigitsOf(location));
-//    }
-//
-//    @Test
-//    public void testNumberOfSentences() {
-//        String location = "C:\\Users\\User\\Videos\\Sample2.txt";
-//        assertEquals(6, Utility.countNumberOfSentencesOf(location));
-//    }
-//
-//    @Test
-//    public void testNumberOfParagraphs() {
-//        String location = "C:\\Users\\User\\Videos\\Sample2.txt";
-//        assertEquals(3, Utility.countNumberOfParagraphsOf(location));
-//    }
-//
-//    @Test
-//    public void testNumberOfParagraphs2() {
-//        String location = "C:\\Users\\User\\Videos\\Sample3.txt";
-//        assertEquals(2, Utility.countNumberOfParagraphsOf(location));
-//    }
-//
-//    @Test
-//    public void testNumberOfParagraphs3() {
-//        String location = "C:\\Users\\User\\ideaProjects\\refactor\\src\\mrFemi\\sample4.txt";
-//        assertEquals(0, Utility.countNumberOfParagraphsOf(location));
-//    }
+    @Test
+    public void testNumberOfDigits() {
+        String location = "C:\\Users\\DELL\\IdeaProjects\\LambdasAndStreams\\src\\Sample.txt";
+        assertEquals(2, Utility.countNumberOfDigitsOf(location));
+    }
+
+    @Test
+    public void testNumberOfSentences() {
+        String location = "C:\\Users\\DELL\\IdeaProjects\\LambdasAndStreams\\src\\Sample2.txt";
+        assertEquals(6, Utility.countNumberOfSentencesOf(location));
+    }
+
+    @Test
+    public void testNumberOfParagraphs() {
+        String location = "C:\\Users\\DELL\\IdeaProjects\\LambdasAndStreams\\src\\Sample2.txt";
+        assertEquals(3, Utility.countNumberOfParagraphsOf(location));
+    }
+
+    @Test
+    public void testNumberOfParagraphs2() {
+        String location = "C:\\Users\\DELL\\IdeaProjects\\LambdasAndStreams\\src\\Sample3.txt";
+        assertEquals(2, Utility.countNumberOfParagraphsOf(location));
+    }
+
+    @Test
+    public void testNumberOfParagraphs3() {
+        String location = "C:\\Users\\DELL\\IdeaProjects\\LambdasAndStreams\\src\\Sample4.txt";
+        assertEquals(0, Utility.countNumberOfParagraphsOf(location));
+    }
 
 }
